4d958dbc5f2a21bc327d33d8177370ea20b77df7
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-22 19:45:46 +0700

    feat(layout): enhance layout component with Google Fonts and improved body styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,16 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title . ' | Learn PHP' ?? 'Learn PHP' }} </title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
-<body>
+<body class="font-sans antialiased">
     <div class="h-full">
         <div class="min-h-full">
             <x-navbar />
             <x-header>{{ $title }}</x-header>
             <main>
-                <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     {{ $slot }}
                 </div>
             </main>
