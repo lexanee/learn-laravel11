5a1cc675481924e214d0cd8f98112e966786c531
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-22 14:09:22 +0700

    feat(view): refactor contact page to use Layout component for improved structure

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-</head>
-
-<body>
-    <h1 class="text-bold text-white">Contact</h1>
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>Contact</x-slot:title>
+    <h3>This is a contact page</h3>
+</x-layout>
