3c53cd68fc0922377966a9aaa6f41e918f6d7948
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-22 22:48:12 +0700

    refactor(blog): simplify blog post links to use a more concise URL structure

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,7 +4,7 @@
         @foreach ($posts as $post)
             <article
                 class="w-full md:w-[calc((100%_-_5rem)/3)] py-8 border border-gray-300 rounded-lg p-6 transition-all duration-300 hover:shadow-lg">
-                <a href="/blog/post/{{ $post['slug'] }}">
+                <a href="/blog/{{ $post['slug'] }}">
                     <h2 class="mb-3 text-3xl font-bold text-gray-900">{{ $post['title'] }}</h2>
                 </a>
                 <div class="text-base text-gray-500">
@@ -13,7 +13,7 @@
                 <p class="my-4 font-light">
                     {{ Str::limit($post['body'], 150) }}
                 </p>
-                <a href="/blog/post/{{ $post['slug'] }}" class="hover:underline transition-all">Read More &raquo;</a>
+                <a href="/blog/{{ $post['slug'] }}" class="hover:underline transition-all">Read More &raquo;</a>
             </article>
         @endforeach
     </div>
