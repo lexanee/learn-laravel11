98fdc528bd6e3ffc30132d29b29709f6858436a7
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-22 14:09:16 +0700

    feat(view): refactor blog page to use Layout component for improved structure

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-
-<body>
-    <h1 class="text-bold">Blog</h1>
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>Blog</x-slot:title>
+    <h3>This is a blog page</h3>
+</x-layout>
