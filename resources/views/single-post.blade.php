21cfa99821b396706188f9fcea6ccf61f9796412
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-26 12:37:33 +0700

    fix(blog): update single post view to use object properties for article data

--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
     <x-slot:title>Article</x-slot:title>
     <article class="py-8 max-w-screen-md">
-        <h2 class="mb-3 text-3xl font-bold text-gray-900">{{ $article['title'] }}</h2>
+        <h2 class="mb-3 text-3xl font-bold text-gray-900">{{ $article->title }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $article['author'] }}</a> |
-            {{ $article['created_at']->diffForHumans() }}
+            <a href="/authors/{{ $article->author->id }}">{{ $article->author->name }}</a> |
+            {{ $article->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light">
-            {{ $article['content'] }}
+            {{ $article->content }}
         </p>
         <a href="/blog" class="hover:underline transition-all">&laquo; Back to all Posts</a>
     </article>
