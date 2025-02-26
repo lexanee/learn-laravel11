704dac6a07e63636d2f18f1d339abcd4f0710aea
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-26 12:36:29 +0700

    feat(model): add author relationship to Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
+    use HasFactory;
     //
+
+    /**
+     * Get the user that owns the Article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
