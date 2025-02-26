4499dfdfdd9877e63a060abaaf5dc927fc944f50
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-26 12:37:27 +0700

    fix(blog): update article view to use object properties and improve author link

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,17 +4,17 @@
         @foreach ($articles as $article)
             <article
                 class="w-full md:w-[calc((100%_-_5rem)/3)] py-8 border border-gray-300 rounded-lg p-6 transition-all duration-300 hover:shadow-lg">
-                <a href="/blog/{{ $article['slug'] }}">
-                    <h2 class="mb-3 text-3xl font-bold text-gray-900">{{ $article['title'] }}</h2>
+                <a href="/blog/{{ $article->slug }}" ">
+                    <h2 class="mb-3 text-3xl font-bold text-gray-900">{{ $article->title }}</h2>
                 </a>
                 <div class="text-base text-gray-500">
-                    <a href="#">{{ $article['author'] }}</a> | {{ $article['created_at']->diffForHumans() }}
+                    <a href="/authors/{{ $article->author->id }}" class="hover:underline">{{ $article->author->name }}</a> | {{ $article->created_at->diffForHumans() }}
                 </div>
                 <p class="my-4 font-light">
-                    {{ Str::limit($article['content'], 150) }}
-                </p>
-                <a href="/blog/{{ $article['slug'] }}" class="hover:underline transition-all">Read More &raquo;</a>
+                    {{ Str::limit($article->content, 150) }}
+                    </p>
+                    <a href="/blog/{{ $article->slug }}" class="hover:underline transition-all">Read More &raquo;</a>
             </article>
-        @endforeach
+ @endforeach
     </div>
 </x-layout>
