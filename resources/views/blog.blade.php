1d2f5627a4e0d0637f8bcd0362844924a44f689e
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-26 19:57:15 +0700

    feat(blog): enhance article display with author username and category link

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,11 +4,14 @@
         @foreach ($articles as $article)
             <article
                 class="w-full md:w-[calc((100%_-_5rem)/3)] py-8 border border-gray-300 rounded-lg p-6 transition-all duration-300 hover:shadow-lg">
+                <p class="text-sm text-gray-500 mb-2">
+                    {{ $article->created_at->diffForHumans() }}
+                </p>
                 <a href="/blog/{{ $article->slug }}" ">
                     <h2 class="mb-3 text-3xl font-bold text-gray-900">{{ $article->title }}</h2>
                 </a>
                 <div class="text-base text-gray-500">
-                    <a href="/authors/{{ $article->author->id }}" class="hover:underline">{{ $article->author->name }}</a> | {{ $article->created_at->diffForHumans() }}
+                    By <a href="/authors/{{ $article->author->username }}" class="hover:underline text-gray-900">{{ $article->author->name }}</a> in <a href="/categories/{{ $article->category->slug }}" class="hover:underline text-gray-900">{{ $article->category->name }}</a>
                 </div>
                 <p class="my-4 font-light">
                     {{ Str::limit($article->content, 150) }}
