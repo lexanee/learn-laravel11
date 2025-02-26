285570332be76a43cb713468f2d02dfd2982da20
lexanee <ir.muhammadfaiz@gmail.com>
2025-02-26 19:57:25 +0700

    feat(routes): add category route and update author route to use username

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Article;
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +19,17 @@ Route::get('/blog', function () {
     ]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
+Route::get('/authors/{user:username}', function (User $user) {
     return view('blog', ['articles' => $user->articles]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('blog', ['articles' => $category->articles]);
 });
 
 Route::get('/blog/{article:slug}', function (Article $article) {
     return view('single-post', ['article' => $article]);
 });
-
 
 Route::get('/contact', function () {
     return view('contact');
